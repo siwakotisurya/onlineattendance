ddb9a031bdb906d26493a579d39a401c85fd31b0
siwakotisurya <siwakotisurya@outlook.com>
2015-10-11 22:18:15 +0545

    make a table in user status page of users view

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,13 @@
 Rails.application.routes.draw do
  
   resources :users do
-      collection do
-    get :user_status
-  end
+    collection do
+      get :user_status
+    end
   end
 
   devise_for :users
   
-   
-
   get 'home'   => 'attendances#home'
 
   get 'aboutus' => 'attendances#about'
